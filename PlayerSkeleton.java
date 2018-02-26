a1d8e24e3ab8898d13aedb22a885e88d3ac13e69
Kennard Ng <Kennard_ng@outlook.com>
2018-02-26 14:32:44 +0800

    update PlayerSkeleton, shift SimulatedState as nested class; change weight variables to array of weights

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -9,14 +9,20 @@ import java.io.IOException;
 public class PlayerSkeleton {
 
 	/********************************* Multipliers to determine value of simulated move *********************************/
-	// Heavily prioritise objective of row clearing.
-	static float rowsClearedMult = 10f;
+	private static final int NUM_PARAMETERS = 5;
+	private static final int ROWS_CLEARED_MULT_INDEX = 0;
+	private static final int GLITCH_COUNT_MULT_INDEX = 1;
+	private static final int BUMPINESS_MULT_INDEX = 2;
+	private static final int TOTAL_HEIGHT_MULT_INDEX = 3;
+	private static final int MAX_HEIGHT_MULT_INDEX = 4;
 
-	// Multipliers used for tiebreakers.
-	static float glitchCountMult = -0.1f;
-	static float bumpinessMult = -0.1f;
-	static float totalHeightMult = -0.5f;
-	static float maxHeightMult = -0.1f;
+	// Heavily prioritise objective of row clearing. Other Multipliers used for tiebreakers.
+	// initialized to default values
+	private static float[] multiplierWeights = {10f, -0.1f, -01.f, -0.5f, -0.1f};
+
+	private static String[] multiplierNames = {
+		"ROWS_CLEARED_MULT", "GLITCH_COUNT_MUL", "BUMPINESS_MUL", "TOTAL_HEIGHT_MUL", "MAX_HEIGHT_MUL"
+	};
 
 	/********************************* End of multipliers *********************************/
 
@@ -133,7 +139,7 @@ public class PlayerSkeleton {
 	private static final String PARAM_FILE_NAME = "parameter.txt";
 
 	/**
-	 * Sets parameter weights for the current iteration. Parameters stored in parameter.txt in same directory as
+	 * Sets parameter multiplierWeights for the current iteration. Parameters stored in parameter.txt in same directory as
 	 * PlayerSkeleton file. If file is empty, then use default parameters.
 	 *
 	 * {@link PlayerSkeleton#setParameters(String[])} for information about how the parameters are set.
@@ -165,17 +171,13 @@ public class PlayerSkeleton {
 	}
 
 	private static void setParameters(String[] values) {
-		rowsClearedMult = Float.parseFloat(values[0]);
-
-		// Multipliers used for tiebreakers.
-		glitchCountMult = Float.parseFloat(values[1]);
-		bumpinessMult = Float.parseFloat(values[2]);
-		totalHeightMult = Float.parseFloat(values[3]);
-		maxHeightMult = Float.parseFloat(values[4]);
+		for (int i = 0; i < NUM_PARAMETERS; i++) {
+			multiplierWeights[i] = Float.parseFloat(values[i]);
+		}
 	}
 
 	/**
-	 * Saves parameter weights of the current iteration. Parameters stored in parameter.txt in same directory as
+	 * Saves parameter multiplierWeights of the current iteration. Parameters stored in parameter.txt in same directory as
 	 * PlayerSkeleton file.
 	 *
 	 * {@link PlayerSkeleton#setParameters(String[])} for information about how the parameters are set.
@@ -186,13 +188,13 @@ public class PlayerSkeleton {
 
 			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
-			// append a newline character.
-			bufferedWriter.write(rowsClearedMult + " "
-					+ glitchCountMult + " "
-					+ bumpinessMult + " "
-					+ totalHeightMult + " "
-					+ maxHeightMult + "\n");
+			String line = "" + multiplierWeights[0];
+			for (int i = 1; i < NUM_PARAMETERS; i++) {
+				line += " " + multiplierWeights[i];
+			}
+			line += "\n";
 
+			bufferedWriter.write(line);
 			bufferedWriter.close();
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -203,150 +205,159 @@ public class PlayerSkeleton {
 	 * Prints value of parameters
 	 */
 	private static void printParameters() {
-		System.out.println("rows-mul: " + rowsClearedMult + " glitch-mul: " + glitchCountMult + " bump-mul: "
-				+ bumpinessMult + " totalHeight-mul: " + totalHeightMult + " maxHeight-mul: " + maxHeightMult);
+		String line = multiplierNames[0] + ": " + multiplierWeights[0];
+
+		for (int i = 1; i < NUM_PARAMETERS; i++) {
+			line += " " + multiplierNames[i] + ": " + multiplierWeights[i];
+		}
+
+		System.out.println(line);
 	}
 	/********************************* End of Parameter weight optimization *********************************/
 
-}
 
-// Creates a simulated state to evaluate the value of doing a move without changing the actual game state
 
-/**
- * {@code SimulatedState} is a simulated state. This helps to evaluate the value of performing a move without altering
- * the game state.
- */
-class SimulatedState extends State {
+	/********************************* Nested class for state simulation *********************************/
 
-	private int field[][];
-	private int top[];
+	/**
+	 * {@code SimulatedState} is a simulated state. This helps to evaluate the value of performing a move without altering
+	 * the game state.
+	 */
+	class SimulatedState extends State {
 
-	public SimulatedState (State s) {
-		// initialize field
-		field = new int[s.getField().length][s.getField()[0].length];
-		for (int i = 0; i < field.length; i++) {
-			for (int j = 0; j < field[0].length; j++) {
-				field[i][j] = s.getField()[i][j];
-			}
-		}
+		private int field[][];
+		private int top[];
 
-		// initialize top
-		top = new int[s.getTop().length];
-		for (int i = 0; i < top.length; i++) {
-			top[i] = s.getTop()[i];
-		}
-
-		nextPiece = s.getNextPiece();
-	}
-
-	// Returns the value of making a move
-	public float getMoveValue(int move[]) {
-		return getMoveValue(move[ORIENT], move[SLOT]);
-	}
-
-	public float getMoveValue(int orient, int slot) {
-		//height if the first column makes contact
-		int height = top[slot]-getpBottom()[nextPiece][orient][0];
-		//for each column beyond the first in the piece
-		for(int c = 1; c < pWidth[nextPiece][orient];c++) {
-			height = Math.max(height,top[slot+c]-State.getpBottom()[nextPiece][orient][c]);
-		}
-
-		// Check if game ended - penalize heavily.
-		if(height+State.getpHeight()[nextPiece][orient] >= ROWS) {
-			return Integer.MIN_VALUE;
-		}
-
-		/********************************* Perform simulation of adding piece *********************************/
-		/********************************* Please ignore this chunk (unless necessary) *********************************/
-		//for each column in the piece - fill in the appropriate blocks
-		for(int i = 0; i < pWidth[nextPiece][orient]; i++) {
-			//from bottom to top of brick
-			for(int h = height+State.getpBottom()[nextPiece][orient][i]; h < height+State.getpTop()[nextPiece][orient][i]; h++) {
-				field[h][i+slot] = 1;
-			}
-		}
-
-		//adjust top
-		for(int c = 0; c < pWidth[nextPiece][orient]; c++) {
-			top[slot+c]=height+State.getpTop()[nextPiece][orient][c];
-		}
-
-		int rowsCleared = 0;
-
-		//check for full rows - starting at the top
-		for(int r = height+State.getpHeight()[nextPiece][orient]-1; r >= height; r--) {
-			//check all columns in the row
-			boolean full = true;
-			for(int c = 0; c < COLS; c++) {
-				if(field[r][c] == 0) {
-					full = false;
-					break;
+		public SimulatedState (State s) {
+			// initialize field
+			field = new int[s.getField().length][s.getField()[0].length];
+			for (int i = 0; i < field.length; i++) {
+				for (int j = 0; j < field[0].length; j++) {
+					field[i][j] = s.getField()[i][j];
 				}
 			}
-			//if the row was full - remove it and slide above stuff down
-			if(full) {
-				rowsCleared++;
-				//for each column
+
+			// initialize top
+			top = new int[s.getTop().length];
+			for (int i = 0; i < top.length; i++) {
+				top[i] = s.getTop()[i];
+			}
+
+			nextPiece = s.getNextPiece();
+		}
+
+		// Returns the value of making a move
+		public float getMoveValue(int move[]) {
+			return getMoveValue(move[ORIENT], move[SLOT]);
+		}
+
+		public float getMoveValue(int orient, int slot) {
+			//height if the first column makes contact
+			int height = top[slot]-getpBottom()[nextPiece][orient][0];
+			//for each column beyond the first in the piece
+			for(int c = 1; c < pWidth[nextPiece][orient];c++) {
+				height = Math.max(height,top[slot+c]-State.getpBottom()[nextPiece][orient][c]);
+			}
+
+			// Check if game ended - penalize heavily.
+			if(height+State.getpHeight()[nextPiece][orient] >= ROWS) {
+				return Integer.MIN_VALUE;
+			}
+
+			/********************************* Perform simulation of adding piece *********************************/
+			/********************************* Please ignore this chunk (unless necessary) *********************************/
+			//for each column in the piece - fill in the appropriate blocks
+			for(int i = 0; i < pWidth[nextPiece][orient]; i++) {
+				//from bottom to top of brick
+				for(int h = height+State.getpBottom()[nextPiece][orient][i]; h < height+State.getpTop()[nextPiece][orient][i]; h++) {
+					field[h][i+slot] = 1;
+				}
+			}
+
+			//adjust top
+			for(int c = 0; c < pWidth[nextPiece][orient]; c++) {
+				top[slot+c]=height+State.getpTop()[nextPiece][orient][c];
+			}
+
+			int rowsCleared = 0;
+
+			//check for full rows - starting at the top
+			for(int r = height+State.getpHeight()[nextPiece][orient]-1; r >= height; r--) {
+				//check all columns in the row
+				boolean full = true;
 				for(int c = 0; c < COLS; c++) {
-
-					//slide down all bricks
-					for(int i = r; i < top[c]; i++) {
-						field[i][c] = field[i+1][c];
+					if(field[r][c] == 0) {
+						full = false;
+						break;
 					}
-					//lower the top
-					top[c]--;
-					while(top[c]>=1 && field[top[c]-1][c]==0)
+				}
+				//if the row was full - remove it and slide above stuff down
+				if(full) {
+					rowsCleared++;
+					//for each column
+					for(int c = 0; c < COLS; c++) {
+
+						//slide down all bricks
+						for(int i = r; i < top[c]; i++) {
+							field[i][c] = field[i+1][c];
+						}
+						//lower the top
 						top[c]--;
+						while(top[c]>=1 && field[top[c]-1][c]==0)
+							top[c]--;
+					}
 				}
 			}
-		}
-		/********************************* End of simulation *********************************/
+			/********************************* End of simulation *********************************/
 
-		int maxHeight = 0;
+			int maxHeight = 0;
 
-		for (int i = 0; i < top.length; i++) {
-			if (top[i] > maxHeight) {
-				maxHeight = top[i];
-			}
-		}
-
-		return PlayerSkeleton.bumpinessMult * getBumpiness(top)
-				+ PlayerSkeleton.totalHeightMult * getTotalHeight(top)
-				+ PlayerSkeleton.rowsClearedMult * rowsCleared
-				+ PlayerSkeleton.maxHeightMult * maxHeight
-				+ PlayerSkeleton.glitchCountMult * getGlitchCount(field);
-	}
-
-	// Checks for how bumpy the top is
-	public float getBumpiness(int[] top) {
-		float bumpiness = 0;
-		for (int i = 0; i < top.length - 1; i++) {
-			bumpiness += Math.abs(top[i] - top[i + 1]);
-		}
-
-		return bumpiness;
-	}
-
-	// Returns the sum of heights
-	public float getTotalHeight(int[] top) {
-		float totalHeight = 0;
-		for (int i = 0; i < top.length; i++) {
-			totalHeight += top[i];
-		}
-
-		return totalHeight;
-	}
-
-	public float getGlitchCount(int[][] field) {
-		float glitchCount = 0;
-		for (int r = 1; r < field.length; r++) {
-			for(int c = 0; c < field[r].length; c++) {
-				if ((field[r][c] == 0) && (field[r - 1][c] != 0)) {
-					glitchCount++;
+			for (int i = 0; i < top.length; i++) {
+				if (top[i] > maxHeight) {
+					maxHeight = top[i];
 				}
 			}
+
+			return multiplierWeights[BUMPINESS_MULT_INDEX] * getBumpiness(top)
+					+ multiplierWeights[TOTAL_HEIGHT_MULT_INDEX] * getTotalHeight(top)
+					+ multiplierWeights[ROWS_CLEARED_MULT_INDEX] * rowsCleared
+					+ multiplierWeights[MAX_HEIGHT_MULT_INDEX] * maxHeight
+					+ multiplierWeights[GLITCH_COUNT_MULT_INDEX] * getGlitchCount(field);
 		}
-		return glitchCount;
+
+		// Checks for how bumpy the top is
+		public float getBumpiness(int[] top) {
+			float bumpiness = 0;
+			for (int i = 0; i < top.length - 1; i++) {
+				bumpiness += Math.abs(top[i] - top[i + 1]);
+			}
+
+			return bumpiness;
+		}
+
+		// Returns the sum of heights
+		public float getTotalHeight(int[] top) {
+			float totalHeight = 0;
+			for (int i = 0; i < top.length; i++) {
+				totalHeight += top[i];
+			}
+
+			return totalHeight;
+		}
+
+		public float getGlitchCount(int[][] field) {
+			float glitchCount = 0;
+			for (int r = 1; r < field.length; r++) {
+				for(int c = 0; c < field[r].length; c++) {
+					if ((field[r][c] == 0) && (field[r - 1][c] != 0)) {
+						glitchCount++;
+					}
+				}
+			}
+			return glitchCount;
+		}
 	}
+
+	/********************************* End of nested class for state simulation *********************************/
 }
+
