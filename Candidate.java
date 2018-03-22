85afbabd0a7213e561d58c41fee6827e2f569d32
fustilio <lfrancis373@gmail.com>
2018-03-22 21:10:55 +0800

    Add AltitudeDifference Heuristic

--- Candidate.java
+++ Candidate.java
@@ -2,20 +2,10 @@ import com.sun.istack.internal.NotNull;
 
 public class Candidate implements Comparable<Candidate>{
     private static final int NUM_PARAMETERS = 5;
-    private static final int ROWS_CLEARED_MULT_INDEX = 0;
-    private static final int GLITCH_COUNT_MULT_INDEX = 1;
-    private static final int BUMPINESS_MULT_INDEX = 2;
-    private static final int TOTAL_HEIGHT_MULT_INDEX = 3;
-    private static final int MAX_HEIGHT_MULT_INDEX = 4;
-
-    private static String[] multiplierNames = {
-            "ROWS_CLEARED_MULT", "GLITCH_COUNT_MUL", "BUMPINESS_MUL", "TOTAL_HEIGHT_MUL", "MAX_HEIGHT_MUL"
-    };
 
     // Heavily prioritise objective of row clearing. Other Multipliers used for tiebreakers.
     // initialized to default values
     private double[] multiplierWeights = {10f, -0.1f, -01.f, -0.5f, -0.1f};
-
     private double fitnessScore = 0;
 
     public Candidate(double[] weights, double fitness) {
